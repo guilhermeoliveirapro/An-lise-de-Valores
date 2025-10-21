9ea61903a1feb9f3dbaeac9089bfccdcab60b150
Guilherme Oliveira <guilhermemattosdev@gmail.com>
2025-10-21 15:06:09 -0300

    Script2.0

--- analiseval.js
+++ analiseval.js
@@ -1,48 +1,59 @@
-const input = document.querySelector('#txtn') // Pega o valor digitado
-const num = []  // Armazena o valor digitado
-const select = document.querySelector('#val')
+const num = document.querySelector('#fnum')
+const lista = document.querySelector('#flista')
 const res = document.querySelector('#res')
+const valores = []
 
-
-const div1 = document.querySelector('.div1')
-const div2 = document.querySelector('.res')
-
-function adicionar(){  // Função que vai armazenar os números em num
-    if (input.value.length === 0) {
-        return alert("[ERRO] VALOR VAZIO, DIGITE UM VALOR");
-    }
-    let valor = Number(input.value)
-    if (valor < 1 || valor > 100) {
-        return alert("[ERRO] VALOR INVÁLIDO, DIGITE UM VALOR")
-    }
-    if (num.indexOf(valor) !== -1) {
-        return alert("[ERRO] VALOR JÁ ADICIONADO")
-    }
-    let item = document.createElement('li')
-    item.textContent = `VALOR ${valor} ADICIONADO`
-    select.appendChild(item)
-    num.push(valor)
-    input.value=''
-    input.focus()
+function isNumero(n){
+    return Number(num.value) > 0 && Number(num.value) <= 100
 }
 
-function finalizar(){ // Função que vai finalizar e gerar os textos
-    if (num.length == 0) {
-        return alert('[ERRO] ADICIONE VALORES ANTES DE FINALIZAR')
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1){
+        return true
+    } else {
+        return false
     }
-    
-    let soma = 0
-    for(let pos in num) {
-        soma += num[pos]
-    }
-    
-    let media = soma / num.length
+}
 
-    res.innerHTML = `
-    <p>AO TODO TEMOS ${num.length} NÚMEROS CADASTRADOS NA LISTA</p>
-    <p>O MAIOR VALOR INFORMADO FOI ${Math.max(...num)}</p>
-    <p>O MENOR VALOR INFORMADO FOI ${Math.min(...num)}</p>
-    <p>SOMANDO TODOS OS VALORES, TEMOS ${soma}</p>
-    <p>A MÉDIA DOS VALORES DIGITADOS É ${media}</p>
-    `
+function adicionar() {
+    if (isNumero(num.value) && !inLista(num.value, valores)){
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Número ${num.value} inserido`
+        lista.appendChild(item)
+        res.innerHTML = ""
+    } else {
+        alert('[ERRO] VALOR INSERIDO É INVÁLIDO OU ESTÁ PRESENTE NA LISTA')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0){
+        alert('[ERRO] ADICIONE VALORES ANTES DE FINALIZAR')
+    } else {
+        let total = valores.length
+        let soma = 0
+        let media = 0
+        let maior = valores[0]
+        let menor = valores[0]
+        
+        for(let pos in valores) {
+            soma += valores[pos]
+            
+            if (valores[pos] > maior)
+                maior = valores[pos]
+
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma/total
+        res.innerHTML = ''
+        res.innerHTML += `<p>${total} valores cadastrados</p>`
+        res.innerHTML += `<p>O maior valor é ${maior}</p>`
+        res.innerHTML += `<p>O menor valor é ${menor}</p>`
+        res.innerHTML += `<p>A soma de todos os valores é ${soma}</p>`
+        res.innerHTML += `<p>A média dos valores é ${media}</p>`
+    }
 }
